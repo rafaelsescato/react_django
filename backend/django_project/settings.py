7828efe07c9b9ca2ba3c23608126bbf80a46a010
rafaelsescato <rafaelsescato@gmail.com>
2024-08-24 08:36:22 -0300

    ADDED[Django]: config to corsheader accept any connections

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -126,3 +126,6 @@ STATIC_ROOT = BASE_DIR / "static"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Cors accept any connections
+CORS_ORIGIN_ALLOW_ALL = True
